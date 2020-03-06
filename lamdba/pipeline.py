15cf26b2e23c59467a48a775b7cd1635a20477d7
paulwallingtontfgm <paul.wallington@tfgm.com>
2020-03-06 17:34:49 +0000

    amended code-pipeline-template.yaml

--- lamdba/pipeline.py
+++ lamdba/pipeline.py
@@ -1,9 +1,13 @@
 import time
+import boto3
+
+pipeline = boto3.client('codepipeline')
 
 
 def invoke_wait(event, context):
 
-    time.sleep(30)
-        
+    time.sleep(5)
 
-
+    response = pipeline.put_job_success_result(
+        jobId=event['CodePipeline.job']['id']
+    )
